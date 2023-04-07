b9479b55e108d6ec6aa0268b4428a32aa7b9dd96
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-04-07 22:38:46 +0700

    menyesuaikan route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->middleware('auth');
+
+//penggunaan middleware di akhir route tambahkan ->middleware('auth')
+
+Route::get('/login', [AuthController::class, 'index']);
+Route::post('/login', [AuthController::class, 'authenticate']);
+Route::get('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'store']);
+Route::post('/logout', [AuthController::class, 'logout']);
