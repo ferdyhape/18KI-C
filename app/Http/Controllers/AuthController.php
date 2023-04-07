29b368d734034a90b3f75b575c6791b7169f3944
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-04-07 22:37:46 +0700

    membuat AuthController dan menyesuaikan middleware auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function register()
+    {
+        return view('auth.register');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('/');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'nama' => 'required|string|min:3|max:255',
+            'alamat' => 'required|string|min:5|max:255',
+            'email' => 'required|email:dns,rfc|unique:users',
+            'password' => 'min:6',
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+        $validatedData = User::create($validatedData);
+
+        return redirect('/login')->with('toast_success', 'Registration is successful, Please login!');
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
