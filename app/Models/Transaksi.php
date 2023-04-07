4da6f0a9e0d74d3d5c238da50dcbdd4464f35e2e
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-04-07 22:00:23 +0700

    update model transaksi menambah relasi ke itemtransaksi

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Itemtransaksi;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -25,5 +26,15 @@ class Transaksi extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get all of the comments for the Transaksi
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function itemtransaksis()
+    {
+        return $this->hasMany(Itemtransaksi::class);
     }
 }
