d9861bd1d8ccf742760760329d92e6827b08f9a4
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-04-07 21:30:17 +0700

    membuat migrasi dan model cart, serta relasi ke user

---
+++ app/Models/Cart.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total_harga',
+    ];
+
+    /**
+     * Get the user that owns the Cart
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
