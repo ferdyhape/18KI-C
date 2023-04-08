6fa5de7525dd4c47cb1d03ae03d9cb3b149b394b
Gufron14 <gupron.nurjalil14@gmail.com>
2023-04-09 01:43:59 +0700

    edit total harga jadi nullable

--- database/migrations/2023_04_07_141850_create_carts_table.php
+++ database/migrations/2023_04_07_141850_create_carts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->integer('total_harga');
+            $table->integer('total_harga')->nullable();
             $table->timestamps();
         });
     }
